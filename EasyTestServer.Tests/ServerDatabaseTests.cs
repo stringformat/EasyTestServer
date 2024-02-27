810ba50f3b5bcd7f99c09a2137e5b66ecbc9426a
agimenez-lucca <145473798+agimenez-lucca@users.noreply.github.com>
2024-02-27 10:26:24 +0100

    WIP

--- EasyTestServer.Tests/ServerDatabaseTests.cs
+++ EasyTestServer.Tests/ServerDatabaseTests.cs
@@ -5,33 +5,33 @@ namespace EasyTestServer.Tests;
 
 public class ServerDatabaseTests
 {
-    [Fact]
-    public async Task Should_ReturnNameFromUser1_When_UseInMemoryDatabaseIsUsedAndWithDataHasAddedUser1()
-    {
-        //arrange
-        var user1 = new User("jean charles");
-        var user2 = new User("jean paul");
-
-        var httpClient = new Server<Program>()
-            .UseLocalDatabase()
-                .WithData(user1)
-                .WithData(user2)
-                .Build<UserContext>(options =>
-                {
-                    options.DbType = LocalDbType.SqlServer;
-                    //options.ConnectionString = "Data Source=../EasyTestServer.Tests/test.db";
-                    options.ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=Test;Trusted_Connection=True;ConnectRetryCount=0;Encrypt=False";
-                })
-            .Build();
-
-        //act
-        var response = await httpClient.GetAsync($"api/users/{user1.Id}");
-        
-        //assert
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        var content = await response.Content.ReadFromJsonAsync<GetUserResponse>();
-        content!.Name.Should().Be("jean charles");
-    }
+    // [Fact]
+    // public async Task Should_ReturnNameFromUser1_When_UseInMemoryDatabaseIsUsedAndWithDataHasAddedUser1()
+    // {
+    //     //arrange
+    //     var user1 = new User("jean charles");
+    //     var user2 = new User("jean paul");
+    //
+    //     var httpClient = new Server<Program>()
+    //         .UseLocalDatabase()
+    //             .WithData(user1)
+    //             .WithData(user2)
+    //             .Build<UserContext>(options =>
+    //             {
+    //                 options.DbType = LocalDbType.SqlServer;
+    //                 //options.ConnectionString = "Data Source=../EasyTestServer.Tests/test.db";
+    //                 options.ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=Test;Trusted_Connection=True;ConnectRetryCount=0;Encrypt=False";
+    //             })
+    //         .Build();
+    //
+    //     //act
+    //     var response = await httpClient.GetAsync($"api/users/{user1.Id}");
+    //     
+    //     //assert
+    //     response.StatusCode.Should().Be(HttpStatusCode.OK);
+    //     var content = await response.Content.ReadFromJsonAsync<GetUserResponse>();
+    //     content!.Name.Should().Be("jean charles");
+    // }
     
     // [Fact]
     // public async Task Should_ReturnFriendFromUser_When_WithDataHasAddedUserWithFriend()
